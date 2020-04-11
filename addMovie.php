40af4bf50ac58f2152bf2504d067533aac4763ec
Ray Garner <psyrg4@nottingham.ac.uk>
2020-04-11 18:53:58 +0100

    added extra fields to add film

--- addMovie.php
+++ addMovie.php
@@ -11,6 +11,10 @@
 <?php
 $movietitle = $_GET['title'];
 $actorname = $_GET['name'];
+$price = $_GET['price'];
+$scenes = $_GET['scenes'];
+$genre = $_GET['genre'];
+$year = $_GET['year'];
 $db_host = 'mysql.cs.nott.ac.uk';
 $db_user = 'psyrg4';
 $db_pass = 'db_pass';
@@ -20,7 +24,8 @@ $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 if ($conn->connect_errno)  die("failed to connect to database\n</body>\n</html>");
 
 //$sql="SELECT mvID,mvTitle,mvPrice,mvYear,mvGenre FROM Movie WHERE mvTitle='$titlesearch'";
-$sql="INSERT INTO Movie (actID, mvTitle) SELECT actID, '$movietitle' FROM Actor WHERE actName='$actorname'";
+//$sql="INSERT INTO Movie (actID, mvTitle) SELECT actID, '$movietitle' FROM Actor WHERE actName='$actorname'";
+$sql="INSERT INTO Movie (actID, mvTitle, mvPrice, mvYear, mvGenre, mvNumScenes) SELECT actID, '$movietitle', $price, $year, '$genre', $scenes FROM Actor WHERE actName='$actorname'";
 $stmt = $conn->prepare($sql);
 $stmt->execute();
 ?>
--- script.js
+++ script.js
@@ -103,3 +103,72 @@ function validate(form) {
 	}
 
 }
+
+function validateNewFilm(form) {
+    var ok = 1;
+    var msg = "";
+
+    for (var i = 0; i < form.length; i++) {
+     
+       
+
+        switch (form.elements[i].name) {
+            case "title":
+                if (form.elements[i].value.trim() == "") {
+                    //msg += "'" + form.elements[i].name + "' is void. "
+                    ok = 0;
+
+                    if (form.elements[i].name == "title") {
+                        alert("'" + form.elements[i].name + "' is void. ");
+                    }
+
+                }
+            case "genre":
+                if (!/^(([a-zA-Z\x20]+)|(\s*))$/.test(form.elements[i].value.trim())) {
+                    //alert("You entered a valid name");
+                    //return true;
+                    //} else {
+                    alert("Name and genre can only contain letters");
+                    //return false;
+                    ok = 0;
+                }
+                break;
+            case "price":
+                if (!/^(([0-9]+\.[0-9][0-9])|(\s*))$/.test(form.elements[i].value.trim())) {
+                  
+                    alert("Invalid price format");
+                    
+                    ok = 0;
+                }
+                break;
+
+            case "year":
+                if (!/^(([1-2][0-9][0-9][0-9])|(\s*))$/.test(form.elements[i].value.trim())) {
+                    alert("Invalid year");
+                    ok = 0;
+                }
+                break;
+            case "scenes":
+                if (!/^(([0-9]+)|(\s*))$/.test(form.elements[i].value.trim())) {
+                    alert("Invalid number of scenes");
+                    ok = 0;
+                }
+                break;
+
+
+        }
+               
+    }
+
+
+    if (ok == 0) {
+        //alert(msg)
+        alert("invalid!")
+        return false
+    }
+    else {
+        alert("valid!");
+        return true
+    }
+
+}
